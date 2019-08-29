e840628116cc7aedd97e60de7c887d7b04cb8f46
Jama Mohamed <jama3137@gmail.com>
2019-08-29 15:02:16 +0300

    Minor fixes

--- app/routes/route.js
+++ app/routes/route.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
 
 const scraperHandler = require('../../app/scrapeWebPage');
 
 dotenv.config();
 const app = express()
+app.use(cors())
+// app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.send('Hello World!')
+})
 
 app.post('/preview', async (req, res) => {
   try {
